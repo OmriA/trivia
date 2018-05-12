c68da24955c077cb036ad71fab0e34c06a0815d0
OfirGoldshtain <ofirgol109@gmail.com>
2018-05-12 22:53:47 +0300

    conclusion: ron doesn't exist (isUserExists works)

--- Trivia/Trivia/DataBase.cpp
+++ Trivia/Trivia/DataBase.cpp
@@ -16,12 +16,12 @@ DataBase::~DataBase()
 
 bool DataBase::isUserExists(string username)
 {
-	string queryString = "SELECT * FROM t_users WHERE username = " + username;
+	string queryString = "SELECT * FROM t_users WHERE username=\"" + username + "\";";
 	const char* query = queryString.c_str();
 	char* errmsg = 0;
 	
 	int data = 0;
-	int succeed = sqlite3_exec(_db, query, callbackCount, (void*)data, &errmsg);
+	int succeed = sqlite3_exec(_db, query, callbackCount, (void*)&data, &errmsg);
 	if (succeed != SQLITE_OK)
 	{
 		cout << "Error! " << errmsg;
@@ -36,6 +36,6 @@ bool DataBase::isUserExists(string username)
 
 int DataBase::callbackCount(void* data, int argc, char** argv, char** azColName)
 {
-	data = (void*)argc;
+	*(int*)data = argc;
 	return 0;
 }
--- Trivia/Trivia/main.cpp
+++ Trivia/Trivia/main.cpp
@@ -1,7 +1,24 @@
 #include "Question.h"
+#include<string>
+#include"DataBase.h"
 
-int main()
+int questiontest()
 {
 	Question first = Question(1, "r u ready?", "yes", "no", "maybe", "aye aye captain");
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	DataBase* db = new DataBase();
+	if (db->isUserExists("ron"))
+	{
+		cout << "true";
+	}
+	else
+	{
+		cout << "false";
+	}
+	system("PAUSE");
 	return 0;
 }
