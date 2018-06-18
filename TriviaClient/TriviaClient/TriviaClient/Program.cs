e1227308cc2fd6431893899798dbb44c9768d355
OfirGoldshtain <ofirgol109@gmail.com>
2018-06-18 18:28:06 +0300

    FINALLY i hope

--- TriviaClient/TriviaClient/TriviaClient/Program.cs
+++ TriviaClient/TriviaClient/TriviaClient/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 namespace TriviaClient
 {
@@ -14,9 +17,32 @@ namespace TriviaClient
         [STAThread]
         static void Main()
         {
+            TcpClient c = new TcpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1906);
+            bool flag = false, flagConnected = false;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            while (!flag && !flagConnected)
+            {
+                try
+                {
+                    c.Connect(serverEndPoint);
+                    flagConnected = true;
+                }
+                catch
+                {
+                    if (!(MessageBox.Show("You do not have a server running\n\nOk - try again.\nCancel - close client.", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes))
+                    {
+                        flag = true;
+                    }
+                }
+            }
+
+            if (flagConnected == true)
+            {
+                Client client = new Client(c);
+                Application.Run(new Title(client));
+            }
         }
     }
 }
